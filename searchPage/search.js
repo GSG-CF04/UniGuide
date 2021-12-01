76d2d2b569d4293447d71fb70c786d1674451062
Yousef <longliveyousefdalloul@gmail.com>
2021-12-01 23:15:28 +0200

    add the menu relates #15

---
+++ searchPage/search.js
@@ -0,0 +1,8 @@
+function show() {
+    var menu = document.getElementById("show");
+    if (menu.style.display === "none") {
+      menu.style.display = "block";
+    } else {
+      menu.style.display = "none";
+    }
+  }
