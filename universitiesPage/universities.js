c91b698364a11c330681c96415c2e24ec226c6c4
nadabassam <nada.b.abu.zaid@gmail.com>
2021-12-02 16:01:39 +0200

    Edit some changes Relates #16

---
+++ universitiesPage/universities.js
@@ -0,0 +1,8 @@
+function fontAwesome() {
+    var css = document.createElement("link");
+    css.href = "https://pro.fontawesome.com/releases/v5.10.0/css/all.css";
+    css.rel = "stylesheet";
+    css.type = "text/css";
+    document.getElementsByTagName("head")[0].appendChild(css);
+}
+fontAwesome();
