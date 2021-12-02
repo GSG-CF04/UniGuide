26b0e2710fe06a8f9b2d704a6568aad29893a202
Yousef <longliveyousefdalloul@gmail.com>
2021-12-02 06:54:56 +0200

    add the menu relates #15

--- aboutPage/about.js
+++ aboutPage/about.js
@@ -1,3 +1,12 @@
+function show() {
+  var menu = document.getElementById("show");
+  if (menu.style.display === "none") {
+    menu.style.display = "block";
+  } else {
+    menu.style.display = "none";
+  }
+}
+
 let slideIndex = 1;
 showSlides(slideIndex);
 
